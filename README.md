32e0b99bc9d21c7bce97234ae5099eb521c23be5
Maxime Blade <gabel.maxime.blade@gmail.com>
2021-07-10 00:45:13 +0200

    Stopped being an idiot

--- README.md
+++ README.md
@@ -1,11 +1,33 @@
-# Keyvee
+# KeyVee
 
-Keyvee is an extended version of [KeyV](https://github.com/lukechilds/keyv) which emits events for each function, and is cross compatible with [KeyV](https://github.com/lukechilds/keyv) providers.
+KeyVee is an extension of [KeyV](https://github.com/lukechilds/keyv) which emits events for each function, and is cross compatible with [KeyV](https://github.com/lukechilds/keyv) providers. All functionality is thus the same as [KeyV](https://github.com/lukechilds/keyv)'s.
+
+## Usage
+
+Install KeyVee
+
+```
+npm install --save keyvee
+```
+
+Create a new KeyVee instance
+
+```js
+// CommonJS
+const KeyVee = require("keyvee");
+
+// ES6 / TypeScript
+import KeyVee from "keyvee";
+
+const keyvee = new KeyVee();
+```
 
 ## Events
 
--   `on("set", function (key) {})`
--   `on("get", function (value) {})`
--   `on("TTL", function ([key, value]) {})`
--   `on("delete", function (key) {})`
--   `on("clear", function () {})`
+```js
+keyvee.emitter.on("set", (key, value) => {});
+keyvee.emitter.on("get", (key) => {});
+keyvee.emitter.on("TTL", (key, value) => {});
+keyvee.emitter.on("delete", (key, value) => {});
+keyvee.emitter.on("clear", () => {});
+```
--- src/index.js
+++ src/index.js
@@ -1,104 +1,41 @@
-const JSONB = require("json-buffer");
+const KeyV = require("keyv");
 const EventEmitter = require("events");
 
-const loadStore = (opts) => {
-    const adapters = {
-        redis: "@keyv/redis",
-        mongodb: "@keyv/mongo",
-        mongo: "@keyv/mongo",
-        sqlite: "@keyv/sqlite",
-        postgresql: "@keyv/postgres",
-        postgres: "@keyv/postgres",
-        mysql: "@keyv/mysql",
-    };
-    if (opts.adapter || opts.uri) {
-        const adapter = opts.adapter || /^[^:]*/.exec(opts.uri)[0];
-        return new (require(adapters[adapter]))(opts);
-    }
-    return new Map();
-};
-
-class Keyvee extends EventEmitter {
-    constructor(uri, opts) {
-        super();
+class KeyVee extends KeyV {
+    constructor(opts) {
+        super(opts);
         this.emitter = new EventEmitter();
-        this.opts = Object.assign(
-            {
-                namespace: "keyv",
-                serialize: JSONB.stringify,
-                deserialize: JSONB.parse,
-            },
-            typeof uri === "string" ? { uri } : uri,
-            opts
-        );
-
-        if (!this.opts.store) {
-            const adapterOpts = Object.assign({}, this.opts);
-            this.opts.store = loadStore(adapterOpts);
-        }
-
-        if (typeof this.opts.store.on === "function") {
-            this.opts.store.on("error", (err) => this.emit("error", err));
-        }
-
-        this.opts.store.namespace = this.opts.namespace;
     }
 
-    _getKeyPrefix(key) {
-        return `${this.opts.namespace}:${key}`;
-    }
-
-    get(key) {
+    async delete(key) {
         const keyPrefixed = this._getKeyPrefix(key);
         const { store } = this.opts;
-        return Promise.resolve()
-            .then((key) => {
-                this.emitter.emit("get", key);
-                store.get(keyPrefixed);
-            })
-            .then((data) => {
-                return typeof data === "string" ? this.opts.deserialize(data) : data;
-            })
-            .then((data) => {
-                if (data === undefined) return undefined;
-                if (typeof data.expires === "number" && Date.now() > data.expires) {
-                    this.emitter.emit("TTL", [key, data.value]);
-                    this.delete(key);
-                    return undefined;
-                }
-                return opts && opts.raw ? data : data.value;
-            });
-    }
-
-    set(key, value, ttl) {
-        const keyPrefixed = this._getKeyPrefix(key);
-        if (typeof ttl === "undefined") ttl = this.opts.ttl;
-        if (ttl === 0) ttl = undefined;
-        const store = this.opts.store;
-
-        return Promise.resolve()
-            .then(() => {
-                const expires = typeof ttl === "number" ? Date.now() + ttl : null;
-                this.emitter.emit("set", key);
-                value = { value, expires };
-                return this.opts.serialize(value);
-            })
-            .then((value) => store.set(keyPrefixed, value, ttl))
-            .then(() => true);
-    }
-
-    delete(key) {
-        this.emitter.emit("delete", key);
-        const keyPrefixed = this._getKeyPrefix(key);
-        const { store } = this.opts;
+        let toDelete = await store.get(keyPrefixed);
+        toDelete = typeof toDelete === "string" ? this.opts.deserialize(toDelete) : toDelete;
+        if (typeof toDelete.expires === "number" && Date.now() > toDelete.expires)
+            this.emitter.emit("TTL", key, toDelete.value);
         return Promise.resolve().then(() => store.delete(keyPrefixed));
     }
 
-    clear() {
+    async eget(key, opts) {
+        this.emitter.emit("get", key);
+        return await this.get(key, opts);
+    }
+
+    async eset(key, value, ttl) {
+        this.emitter.emit("set", key, value);
+        return await this.set(key, value, ttl);
+    }
+
+    async edelete(key) {
+        this.emitter.emit("delete", key, await this.get(key));
+        return await this.delete(key);
+    }
+
+    async eclear() {
         this.emitter.emit("clear");
-        const { store } = this.opts;
-        return Promise.resolve().then(() => store.clear());
+        return await this.clear();
     }
 }
 
-module.exports = Keyvee;
+module.exports = KeyVee;
